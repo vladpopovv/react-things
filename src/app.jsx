21d0d430a654eb796467e8f74425a039b829104d
Vlad Popov <vllad1108@gmail.com>
2019-06-02 01:38:48 +0600

    CHANGE: Rewring sidebar component to class component. ADD:  closing sidebar by ESC key

--- src/app.jsx
+++ src/app.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import { Provider } from 'react-redux';
 import { ConnectedRouter } from 'connected-react-router';
+import { Link } from 'react-router-dom';
 import createStore from './store/createStore';
 import history from './history';
 import Sidebar from './components/sidebar';
@@ -12,8 +13,14 @@ ReactDom.render(
   <Provider store={store}>
     <ConnectedRouter history={history}>
       <div className="root-container">
-        <Sidebar />
-        <h1>Hello, world!</h1>
+        <Sidebar>
+          <nav>
+            <Link href="/" to="/">
+              Main page
+            </Link>
+          </nav>
+        </Sidebar>
+        <h1 className="main-title">Hello, I'm Vlad!</h1>
       </div>
     </ConnectedRouter>
   </Provider>,
--- src/components/sidebar/index.js
+++ src/components/sidebar/index.js
@@ -4,37 +4,60 @@ import { connect } from 'react-redux';
 import classNames from 'classnames';
 import AppAction from 'actions/app';
 
-function Sidebar(props) {
-  const { isOpen, toogleSidebar } = props;
+class Sidebar extends React.Component {
+  componentDidMount() {
+    document.addEventListener('keydown', this.escHandler, false);
+  }
 
-  const closeSidebar = () => toogleSidebar(false);
+  componentWillUnmount() {
+    document.removeEventListener('keydown', this.escHandler, false);
+  }
 
-  return (
-    <div className={classNames('sidebar', { 'sidebar--open': isOpen })}>
-      <div
-        className="sidebar-overlay"
-        onClick={closeSidebar}
-        onKeyDown={closeSidebar}
-        role="button"
-        tabIndex="0"
-      />
-      <div className="sidebar-body">
-        <button
-          className="sidebar-toggle"
-          onClick={() => toogleSidebar(!isOpen)}
-          type="button"
-        >
-          <div className={classNames('burger-icon', { 'burger-cross': isOpen })} />
-        </button>
-        Sidebar
+  closeSidebar = () => {
+    const { toogleSidebar } = this.props;
+
+    toogleSidebar(false);
+  }
+
+  escHandler = (e) => {
+    const { isOpen } = this.props;
+
+    if (e.keyCode === 27 && isOpen) {
+      this.closeSidebar();
+    }
+  }
+
+  render() {
+    const { isOpen, toogleSidebar, children } = this.props;
+
+    return (
+      <div className={classNames('sidebar', { 'sidebar--open': isOpen })}>
+        <div
+          className="sidebar-overlay"
+          onClick={this.closeSidebar}
+          onKeyDown={this.closeSidebar}
+          role="button"
+          tabIndex="0"
+        />
+        <div className="sidebar-body">
+          <button
+            className="sidebar-toggle"
+            onClick={() => toogleSidebar(!isOpen)}
+            type="button"
+          >
+            <div className={classNames('burger-icon', { 'burger-cross': isOpen })} />
+          </button>
+          {children}
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 Sidebar.propType = {
   isOpen: PropTypes.bool,
   toogleSidebar: PropTypes.func,
+  children: PropTypes.element,
 };
 
 export default connect(state => ({
